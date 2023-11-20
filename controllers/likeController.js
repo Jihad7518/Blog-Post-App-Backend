f55bcd4f3c50a91e12f399987fc5352f977dadd9
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-21 00:13:35 +0600

    Update likeController.js

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,5 +1,4 @@
 
-
 //import models
 const Post = require("../models/postModel");
 const Like = require("../models/likeModel");
@@ -28,3 +27,32 @@ exports.likePost = async (req,res) => {
         });
     }
 };
+
+
+//unlike a post
+exports.unlikePost = async (req,res) => {
+    try{
+            const {post, like} = req.body;
+            
+            //find and delete the id from post and like 
+            const deletedLike = await Like.findByIdAndDelete({post:post, _id:like} );
+
+            //update post
+            const updatedPost = await Post.findByIdAndUpdate(post, {$pull: {likes:deletedLike._id}}, {new: true} );
+
+            res.json({
+                post:updatedPost,
+            });
+    }
+    catch(error) {
+        return res.status(400).json({
+            error: "Error while uliking post",
+        });
+    }
+};
+
+
+exports.dummyLink = (req,res) => {
+    res.send("This is Dummy Page");
+}
+
